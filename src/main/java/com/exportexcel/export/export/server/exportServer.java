e1d30c14035fbb6e2028985361fbef20dfade55d
root <1285800895@qq.com>
2020-12-22 16:45:39 +0800

    123

--- src/main/java/com/exportexcel/export/export/server/exportServer.java
+++ src/main/java/com/exportexcel/export/export/server/exportServer.java
@@ -6,6 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +15,18 @@ import java.util.Map;
 @Component
 @EnableScheduling
 public class exportServer {
-    @Autowired
+    @Autowired(required = false)
     private PmTenantUserMapper pmTenantUserMapper;
-    @Scheduled(cron = "30 31 15 * * ?")
+    @Scheduled(cron = "30 * * * * ?")
     public void export() {
         Map queryMap = new HashMap();
+        Calendar yesterdayStart = Calendar.getInstance();
+        yesterdayStart.add(Calendar.DATE,-2);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        yesterdayStart.set(Calendar.HOUR, 23);
+        yesterdayStart.set(Calendar.MINUTE, 58);
+        yesterdayStart.set(Calendar.SECOND, 59);
+        System.out.println(sdf.format(yesterdayStart.getTime()));
         List<Map> selectTaskList = pmTenantUserMapper.selectTaskList(queryMap);
 
         System.out.println("hello world");
