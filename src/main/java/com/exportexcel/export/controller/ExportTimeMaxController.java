6b7bde1b27309604e795bac3856fd9be0cbf38f6
root <1285800895@qq.com>
2021-05-12 13:29:48 +0800

    v5.0

---
+++ src/main/java/com/exportexcel/export/controller/ExportTimeMaxController.java
@@ -0,0 +1,26 @@
+package com.exportexcel.export.controller;
+
+import com.exportexcel.export.server.ExportTimeMaxService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+
+/**
+ * @author: 12858
+ * @date: 2021.05.12
+ * @Description: 导出时间段最大打点数
+ * @Version: 1.0
+ */
+public class ExportTimeMaxController {
+    @Autowired(required = false)
+    private ExportTimeMaxService exportTimeMaxService;
+
+    @RequestMapping(value = "exportTimeMaxService")
+    public void export(HttpServletRequest request, HttpServletResponse response) throws FileNotFoundException {
+        exportTimeMaxService.exportTimeMaxService(request, response);
+    }
+
+}
